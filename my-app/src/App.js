ed114e10df52ff06133087a91be5a13f4c71aa26
Heather Goodwin <158013998+HGoodwin415@users.noreply.github.com>
2024-07-21 09:27:55 -0500

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import './App.css';
+import Footer from './Footer';
+import MainContent from './MainContent';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <MainContent /> 
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
